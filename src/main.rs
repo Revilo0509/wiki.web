fe109d9aec0d43b7c4f9f23613198243be54552e
Revilo0509 <olivers_gamecenter@outlook.com>
2025-09-10 22:58:14 +0200

    fu

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use tokio::sync::RwLock;
 
 const DEV_MODE: bool = true;
 const PORT: &str = "4878";
-const DOMAIN: &str = "192.168.0.12";
+const DOMAIN: &str = "0.0.0.0";
 
 type ComponentCache = Arc<RwLock<HashMap<&'static str, String>>>;
 
